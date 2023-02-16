a8cf7ef8b175b0726de4598b29e3a05b0d9bef92
FranciscoFoz <64700794+FranciscoFoz@users.noreply.github.com>
2023-02-16 20:37:15 -0300

    Inserindo script de coleta de dados

---
+++ coleta_dados.py
@@ -0,0 +1,34 @@
+#COLETANDO DADOS DA BIBLIOTECA DE SEATTLE
+
+#Bibliotecas
+import requests
+import os
+
+
+#Caminho do diretório onde o arquivo será salvo
+diretorio = 'datasets'
+
+#Definindo primeira linha dos arquivos
+primeira_linha = ['UsageClass', 'CheckoutType', 'MaterialType', 'CheckoutYear', 'CheckoutMonth', 
+                  'Checkouts', 'Title', 'ISBN', 'Creator', 'Subjects', 'Publisher', 'PublicationYear']
+
+# Criar um loop para extrair os dados
+limite = 0
+
+while limite < 42033208:
+    # Criar a URL com o limite
+    url = f'https://data.seattle.gov/resource/tmmm-ytt6.json?$limit=1000&$offset={limite}'
+    # Fazer a requisição
+    response = requests.get(url)
+    dados = response.json()
+
+    # Abrir o arquivo
+    caminho_arquivo = os.path.join(diretorio, f'arquivo{limite}.csv')
+
+    with open(caminho_arquivo, 'w') as arquivo:
+        arquivo.write(';;'.join(primeira_linha) + '\n')
+        for linha in dados:
+            arquivo.write(';;'.join(str(valor) for valor in linha.values()) + '\n')
+
+    # Atualizar o limite
+    limite += 1000
